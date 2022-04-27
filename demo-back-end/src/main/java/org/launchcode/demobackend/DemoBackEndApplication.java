346be5c6cae9d33bead6b0d07a38b81047f8dcfd
Carolista <cjones.renaissancewoman@gmail.com>
2022-04-27 04:11:56 -0500

    Set up back end Java/Spring/Gradle project

---
+++ demo-back-end/src/main/java/org/launchcode/demobackend/DemoBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.demobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackEndApplication.class, args);
+	}
+
+}
---
+++ demo-back-end/src/test/java/org/launchcode/demobackend/DemoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.demobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
