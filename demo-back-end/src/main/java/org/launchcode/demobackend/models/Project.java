aa82408f5a4a419cb42e222a1ef1917ba65a42d6
Carolista <cjones.renaissancewoman@gmail.com>
2022-04-27 05:28:25 -0500

    added Project model

---
+++ demo-back-end/src/main/java/org/launchcode/demobackend/models/Project.java
@@ -0,0 +1,48 @@
+package org.launchcode.demobackend.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Project extends NamedEntity {
+
+    private String roomType;
+    private double roomLength;
+    private double roomWidth;
+    private double roomHeight;
+
+    public Project() {}
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getRoomLength() {
+        return roomLength;
+    }
+
+    public void setRoomLength(double roomLength) {
+        this.roomLength = roomLength;
+    }
+
+    public double getRoomWidth() {
+        return roomWidth;
+    }
+
+    public void setRoomWidth(double roomWidth) {
+        this.roomWidth = roomWidth;
+    }
+
+    public double getRoomHeight() {
+        return roomHeight;
+    }
+
+    public void setRoomHeight(double roomHeight) {
+        this.roomHeight = roomHeight;
+    }
+
+}
+
