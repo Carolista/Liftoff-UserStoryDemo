903a71f9c89d456d40d808de2cd2cb1e917503c8
Carolista <cjones.renaissancewoman@gmail.com>
2022-04-27 05:33:13 -0500

    added project data repository

---
+++ demo-back-end/src/main/java/org/launchcode/demobackend/models/data/ProjectRepository.java
@@ -0,0 +1,10 @@
+package org.launchcode.demobackend.models.data;
+
+import org.launchcode.demobackend.models.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Integer> {
+
+}
