fd86dda8832a23c56d591db141b928efcb9ce561
Carolista <cjones.renaissancewoman@gmail.com>
2022-04-27 20:33:19 -0500

    Added create-project component with form and POST call to API

--- demo-front-end/src/app/app.module.ts
+++ demo-front-end/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CreateProjectComponent } from './project/create-project/create-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
