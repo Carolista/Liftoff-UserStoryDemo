67d85d6e81b496dcacdbf82758576eddc331f27d
Carolista <cjones.renaissancewoman@gmail.com>
2022-04-28 23:13:48 -0500

    added project class and create-project component

--- demo-front-end/src/app/app.module.ts
+++ demo-front-end/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateProjectComponent } from './projects/create-project/create-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProjectComponent
   ],
   imports: [
     BrowserModule,
