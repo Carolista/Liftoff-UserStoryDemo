6256124a595be30888ef0352ff662552acc13c22
Carolista <cjones.renaissancewoman@gmail.com>
2022-04-28 23:49:24 -0500

    added form and handlers etc. to create-project component

--- demo-front-end/src/app/app.module.ts
+++ demo-front-end/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateProjectComponent } from './projects/create-project/create-project.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CreateProjectComponent } from './projects/create-project/create-project
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
